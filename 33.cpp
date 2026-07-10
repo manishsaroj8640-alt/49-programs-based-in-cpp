73f3cd612172919a46f0a733abd90ae880013a06
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-11 02:11:54 +0530

    Delete 33.cpp

--- 33.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int num1 = 5,num2 = 7;
-     int sum = num1 + num2;
-    cout<< " the sum of " << num1 << " and " << num2 <<" is "<< sum << endl;
-    return 0;
-}
