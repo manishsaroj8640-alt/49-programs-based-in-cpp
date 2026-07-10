0c18718757100db934eafebf043b5c88065926d3
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-11 02:04:39 +0530

    sum of two numbers

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 5,num2 = 7;
+     int sum = num1 + num2;
+    cout<< " the sum of " << num1 << " and " << num2 <<" is "<< sum << endl;
+    return 0;
+}
