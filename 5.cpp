d87264fc6f28dd9e8def0d17615b1f3c77d14e43
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-11 02:29:44 +0530

    multiplication table in cpp

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number,i;
+    cout << "enter a number: " << endl;
+    cin >> number;
+    cout << " multiplication table " << number << "/n" << endl;
+for(int i = 1; i <= 10; ++i)
+{
+    cout << number << "*"<<i<<"="<<(number * i)<< endl;
+}
+return 0;
+}
