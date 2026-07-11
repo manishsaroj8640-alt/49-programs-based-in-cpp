495f69b2bc9d45f60ce3b78d8c277710eb7c0921
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-12 01:41:50 +0530

    header in c++

---
+++ 7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    cout<<"using for iostream's cout to print"<<endl;
+    vector<int> v{11,22,550};
+    cout<<"using vector container:"<<endl;
+    for(auto i:v)
+    cout<<i<<" ";
+    return 0;
+    
+}
