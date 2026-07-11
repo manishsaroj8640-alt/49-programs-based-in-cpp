0ff1273c86de91d2fa69a3fc5ad4cde62031100d
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-12 02:11:41 +0530

    function in c++

---
+++ 9.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int add(int num1,int num2)
+{
+    return num1 + num2;
+}
+void greet(){
+    cout<<"hello from the greet function:/n"<<endl;
+    }
+    int main()
+        
+    { 
+          int result = add(45,68);
+
+    cout << "sum:" << result << endl;
+
+greet();
+     
+    return 0;
+            }
