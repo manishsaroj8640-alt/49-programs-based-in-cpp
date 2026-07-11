7ab93b5b600affa19e529a71c5c22ae512333e9e
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-12 00:27:49 +0530

    hello world

--- 6.cpp
+++ 6.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
  int main()
@@ -11,18 +11,3 @@ int last_digit = fourdigitnumber%10;
 cout<<"first digit:"<<first_digit<<endl;
 cout<<"last digit:"<<last_digit<<endl;
 return 0;
-=======
-#include<iostream>
-using namespace std;
- int main()
- {
-    int fourdigitnumber;
-cout<< "enter four digit numbers";
-cin>> fourdigitnumber;
-int first_digit = fourdigitnumber/1000;
-int last_digit = fourdigitnumber%10;
-cout<<"first digit:"<<first_digit<<endl;
-cout<<"last digit:"<<last_digit<<endl;
-return 0;
->>>>>>> origin/programs
- }
