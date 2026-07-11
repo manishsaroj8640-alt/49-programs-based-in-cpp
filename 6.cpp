c476df3d2a92cddd65cacf5cf51c70c69df0da7d
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-12 00:56:26 +0530

    first and last digit of fourdigitnumber

---
+++ 6.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int fourdigitnumber;
+cout<< "enter four digit numbers";
+cin>> fourdigitnumber;
+int first_digit = fourdigitnumber/1000;
+int last_digit = fourdigitnumber%10;
+cout<<"first digit:"<<first_digit<<endl;
+cout<<"last digit:"<<last_digit<<endl;
+return 0;
+ }
