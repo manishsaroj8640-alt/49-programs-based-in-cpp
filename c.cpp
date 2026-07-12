8241dcf6f7b88d47b86d341b7ca998fbeb312687
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-12 14:28:38 +0530

    arithmetic operation

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1 = 10;
+    int num2 = 5;
+    int sum = num1 + num2;
+    cout<< "sum :"<< sum << endl;
+    int difference = num1 - num2;
+    cout<< "difference :"<<difference<<endl;
+    int product = num1*num2;
+    cout<<"product :"<<product<<endl;
+    int quotient =  num1/num2;
+cout << "quotient :"<<quotient<<endl;
+return 0;
+}
