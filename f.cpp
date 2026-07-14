b0d8f12715e1c8b961acd27d3f6359e183d1d724
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-14 22:34:18 +0530

    comparison operators

---
+++ f.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=100;
+    int num2=200;
+
+ if (num1 == num2){
+                   cout<<"equal to \n"<< endl;
+    } else{
+                 cout<<"not equal\n"<<endl;
+    }
+ if(num1!=num2){
+                 cout<<"not equal to\n"<<endl;
+    }else{
+                 cout<<"is equal to\n"<<endl;
+    }
+ if(num1>num2){
+                 cout<<"is greater than\n"<<endl;
+    }else{
+                 cout<<"is not greater than\n"<<endl;
+    }
+if(num1<num2){
+                 cout<<"is less than\n"<<endl;
+}else{
+                 cout<<"is not less than\n"<<endl;
+}
+ return 0;
+
+}
