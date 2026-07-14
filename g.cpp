bfbfe3ab5b20ca73ccdedeba8f60aec6871ab121
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-14 22:57:54 +0530

    if statement

---
+++ g.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"enter a number:"<<endl;
+    cin>>number;
+    if(number>0){
+        cout<<"the number is positive"<<endl;
+    }else if(number<0){
+        cout<<"the number is negative"<<endl;
+    }else{
+        cout<<"the number is zero"<<endl;
+    }
+    return 0;
+}
