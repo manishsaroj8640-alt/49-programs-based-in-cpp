df9fe15a1c223702b9d4c5aaad96fb3e017e551c
manishsaroj8640-alt! <manishsaroj8640@gmail.com>
2026-07-15 00:07:59 +0530

    if else

---
+++ h.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"enter a number"<<endl;
+    cin>>number;
+    if(number%2==0){
+        cout<<"enter number"<<endl;
+    }else{
+        cout<<"odd number"<<endl;
+    }
+    return 0;
+}
