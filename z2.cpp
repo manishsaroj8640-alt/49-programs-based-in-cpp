cbb4a72be370aecdf62a73d82b5758a9ffeeb10c
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 00:24:41 +0530

    numeric data type 2

---
+++ z2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float price = 25.99;
+    double preciseprice = 99.999;
+    cout<<"price:$"<<price<<endl;
+    cout<<"precise price:$"<<preciseprice<<endl;
+    return 0;
+}
