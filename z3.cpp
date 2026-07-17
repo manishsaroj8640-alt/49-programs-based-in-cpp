11702abaaa30acfa2025ea36520e53b526474048
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 00:30:53 +0530

    boolean data type

---
+++ z3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool isCodingFun = true;
+    if(isCodingFun){
+        cout<<"C++ programming code is fun"<<endl;
+    }else{
+        cout<<"coding is not for fun!"<<endl;
+    }
+    return 0;
+}
