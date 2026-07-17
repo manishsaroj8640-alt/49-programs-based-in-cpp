96624b6c7e31615bc3d8ad509c756d3a5cce2efd
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-17 23:32:30 +0530

    declaring variable

---
+++ v.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    float price;
+    age=28;
+    price = 100.00;
+cout<<"age:"<<age<<endl;
+cout<<"price:$"<<price<<endl;
+return 0;
+}
