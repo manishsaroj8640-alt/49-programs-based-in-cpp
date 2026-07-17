c9b0ab1bf9ec69588627ba2f48cc4b210f08f462
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 00:19:43 +0530

    numeric data type

---
+++ z.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=25;
+    short shortage=30;
+    long longage=40;
+    cout<<"age:"<<age<<endl;
+    cout<<"shortage:"<<shortage<<endl;
+    cout<<"longage:"<<longage<<endl;
+    return 0;
+}
