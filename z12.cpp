b59ca688b7a0040e03e8c986f91ff8a7d10445af
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 19:14:50 +0530

    special character

---
+++ z12.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout<<"This is a line with a new line character\n";
+    cout<<"Tabbed text:\t Hello,C++!";
+cout<<"Backslash:This is backslash:|||\n";
+return 0;
+}
