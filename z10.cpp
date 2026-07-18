751a29fc24a1964157fc11fec0c4ddb7911b6db7
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 18:48:45 +0530

    access string

---
+++ z10.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string message = " coding is fun! ";
+for(char character : message){
+    cout<<character<<" ";
+}
+return 0;
+}
