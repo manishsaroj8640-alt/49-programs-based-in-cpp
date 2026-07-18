4e4daf7e179e1672e0face0d9b8d4e0dd8b4005c
Manish kumar Saroj <manishsaroj8640@gmail.com>
2026-07-18 18:59:52 +0530

    access string

---
+++ z11.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string sentence = "programming if fascinating!";
+string Keyword = sentence . substr (0 , 11);
+cout<<"keyword: "<<Keyword<<endl;
+return 0;
+}
